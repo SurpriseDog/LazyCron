c0d0a762eb2374704f88a487e062ded29d6ad720
benruss64@gmail.com <49625187+SurpriseDog@users.noreply.github.com>
2023-06-07 16:30:48 -0500

    lc.launcher.sh

---
+++ lc.launcher.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Launch LazyCron from a limited environment like crontab
+
+
+ARGS="$*"
+cd `dirname "$0"`
+
+
+
+# Setup Logging
+LOGS="/tmp/lazycron_logs.txt"
+truncate -s 0 $LOGS
+elog(){ echo "ELOG $*"; echo "ELOG $*" >> "$LOGS"; }
+
+
+# Choose correct display
+DISPLAY=$(w | grep `whoami` | awk '{print $3}')
+if [[ -z "$DISPLAY" ]]; then
+	elog "Unable to find Display, defaulting to 0"
+	DISPLAY=:0
+fi
+export DISPLAY=$DISPLAY
+
+
+# Setup Path
+export PATH=`sed 's/PATH=//' /etc/environment`
+
+
+# Log status
+elog "Current directory: $PWD"
+elog "Current user: $USER"
+elog "Display = $DISPLAY"
+elog "Path = $PATH"
+elog "Logs = $LOGS"
+elog "Arguments = $ARGS"
+elog "Currently logged in:"
+w >> "$LOGS"
+elog ""
+elog ""
+
+
+# Wait
+echo "Sleeping at boot to be nice"
+sleep 8
+
+
+
+
+# Run Program
+if [[ -z `whereis zenity` ]]; then
+	elog "Install zenity to get desktop notifications if LC crashes."
+	./LazyCron.py $ARGS >> "$LOGS" 2>&1
+else
+	./LazyCron.py $ARGS >> "$LOGS" 2>&1 || zenity --info "info" --timeout=99999999 --text="LazyCron error at `date +%H:%M`"
+fi
